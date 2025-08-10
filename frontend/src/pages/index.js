fa2bc66aefff721bbdad7c6156661a103b326656
shaun <modipanesh@gmail.com>
2025-08-10 07:57:04 +0200

    fix: import error

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,4 +1,4 @@
 export {default as Register} from './Register';
 export {default as NotFound} from './NotFound';
 export {default as Explore} from './Explore';
-export {default as LoginPage} from './login';
+export {default as LoginPage} from './Login';
